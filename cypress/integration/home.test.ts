f3cf6ae99b42cd2c9184359a7078dfb32bb7ddad
Curtis Timson <curt@live.co.uk>
2021-12-23 19:36:38 +0000

    add test for theme credit

--- cypress/integration/home.test.ts
+++ cypress/integration/home.test.ts
@@ -5,11 +5,16 @@
 
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
-    // Start from the index page
     cy.visit('http://localhost:3000/');
 
     cy.get('.site-logo')
       .should('have.attr', 'href')
       .and('include', '/');
+
+    cy.get('footer')
+      .contains('StyleShout')
+      .should('have.attr', 'href')
+      .and('include', 'https://www.styleshout.com/');
+
   });
 });
