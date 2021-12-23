ab651002d9aa39b3f58bef6c18e45be3b46983f0
Curtis Timson <curt@live.co.uk>
2021-12-23 14:32:19 +0000

    add more tests for Post component

--- components/PostLink.test.tsx
+++ components/PostLink.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import PostLink from './PostLink';
 
 describe('PostLink', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     render(<PostLink />);
   });
 
@@ -13,5 +13,21 @@ describe('PostLink', () => {
     });
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it('renders Design tag', () => {
+    const designTag = screen.getByRole('link', {
+      name: 'Design',
+    });
+
+    expect(designTag).toBeInTheDocument();
+  });
+
+  it('renders Design tag href', () => {
+    const designTag = screen.getByRole('link', {
+      name: 'Design',
+    });
+
+    expect(designTag).toHaveAttribute('href', 'category.html');
   });
 });
