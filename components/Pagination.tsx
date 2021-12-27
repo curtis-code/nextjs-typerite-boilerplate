dc693a8f68fdaf68677ae46f9365f5b0270b76f9
Curtis Timson <curt@live.co.uk>
2021-12-27 09:38:11 +0000

    add PageLink

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+interface IPageLink {
+  isCurrentPage?: boolean;
+  pageNumber: number;
+}
 interface IPagination {
   currentPage: number;
   pageCount: number;
 }
+
+function PageLink({ pageNumber, isCurrentPage }: IPageLink) {
+  return (
+    <li>
+      {isCurrentPage
+        ? <span className="pgn__num current">{pageNumber}</span>
+        : <a className="pgn__num" href={`/posts/${pageNumber}`}>{pageNumber}</a>}
+    </li>
+  );
+}
+
+PageLink.defaultProps = {
+  isCurrentPage: false,
+};
 
 export default function Pagination({ currentPage, pageCount }: IPagination) {
   const isLastPage = currentPage === pageCount;
@@ -12,13 +30,13 @@ export default function Pagination({ currentPage, pageCount }: IPagination) {
     <nav className="pgn">
       <ul>
         {currentPage > 1 && <li><a className="pgn__prev" href="#0">Prev</a></li>}
-        <li><a className="pgn__num" href="#0">1</a></li>
-        <li><span className="pgn__num current">2</span></li>
-        <li><a className="pgn__num" href="#0">3</a></li>
-        <li><a className="pgn__num" href="#0">4</a></li>
-        <li><a className="pgn__num" href="#0">5</a></li>
+        <PageLink pageNumber={1} />
+        <PageLink pageNumber={2} isCurrentPage />
+        <PageLink pageNumber={3} />
+        <PageLink pageNumber={4} />
+        <PageLink pageNumber={5} />
         <li><span className="pgn__num dots">…</span></li>
-        <li><a className="pgn__num" href="#0">8</a></li>
+        <PageLink pageNumber={8} />
         {!isLastPage && <li><a className="pgn__next" href="#0">Next</a></li>}
       </ul>
     </nav>
