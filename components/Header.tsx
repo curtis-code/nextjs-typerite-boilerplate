0bb9a8378fc3e3bc576214f0c8bde4ed5cdb986d
Curtis Timson <curt@live.co.uk>
2021-12-29 17:04:48 +0000

    add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="s-header">
+
+      <div className="header__top">
+        <div className="header__logo">
+          <Link href="/">
+            <a className="site-logo">
+              <img src="/images/logo.svg" alt="Homepage" />
+            </a>
+          </Link>
+        </div>
+      </div>
+
+      <nav className="header__nav-wrap">
+        <ul className="header__nav">
+          <li className="current"><Link href="/">Home</Link></li>
+          <li className="has-children">
+            <a href="#0" title="">Categories</a>
+            <ul className="sub-menu">
+              <li><a href="category.html">Lifestyle</a></li>
+              <li><a href="category.html">Health</a></li>
+              <li><a href="category.html">Family</a></li>
+              <li><a href="category.html">Management</a></li>
+              <li><a href="category.html">Travel</a></li>
+              <li><a href="category.html">Work</a></li>
+            </ul>
+          </li>
+          <li className="has-children">
+            <a href="#0" title="">Blog Posts</a>
+            <ul className="sub-menu">
+              <li><a href="single-video.html">Video Post</a></li>
+              <li><a href="single-audio.html">Audio Post</a></li>
+              <li><a href="single-gallery.html">Gallery Post</a></li>
+              <li><a href="single-standard.html">Standard Post</a></li>
+            </ul>
+          </li>
+          <li><a href="styles.html" title="">Styles</a></li>
+          <li><a href="page-about.html" title="">About</a></li>
+          <li><a href="page-contact.html" title="">Contact</a></li>
+        </ul>
+
+        <ul className="header__social">
+          <li className="ss-facebook">
+            <a href="https://facebook.com/">
+              <span className="screen-reader-text">Facebook</span>
+            </a>
+          </li>
+          <li className="ss-twitter">
+            <a href="#0">
+              <span className="screen-reader-text">Twitter</span>
+            </a>
+          </li>
+          <li className="ss-dribbble">
+            <a href="#0">
+              <span className="screen-reader-text">Dribbble</span>
+            </a>
+          </li>
+          <li className="ss-pinterest">
+            <a href="#0">
+              <span className="screen-reader-text">Behance</span>
+            </a>
+          </li>
+        </ul>
+
+      </nav>
+
+      <a href="#0" className="header__menu-toggle">
+        <span>Menu</span>
+      </a>
+
+    </header>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Head from 'next/head';
-import Link from 'next/link';
+import Header from '../components/Header';
 import Search from '../components/Search';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -39,77 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       <div id="top" className="s-wrap site-wrapper">
 
-        <header className="s-header">
-
-          <div className="header__top">
-            <div className="header__logo">
-              <Link href="/">
-                <a className="site-logo">
-                  <img src="/images/logo.svg" alt="Homepage" />
-                </a>
-              </Link>
-            </div>
-          </div>
-
-          <nav className="header__nav-wrap">
-            <ul className="header__nav">
-              <li className="current"><Link href="/">Home</Link></li>
-              <li className="has-children">
-                <a href="#0" title="">Categories</a>
-                <ul className="sub-menu">
-                  <li><a href="category.html">Lifestyle</a></li>
-                  <li><a href="category.html">Health</a></li>
-                  <li><a href="category.html">Family</a></li>
-                  <li><a href="category.html">Management</a></li>
-                  <li><a href="category.html">Travel</a></li>
-                  <li><a href="category.html">Work</a></li>
-                </ul>
-              </li>
-              <li className="has-children">
-                <a href="#0" title="">Blog Posts</a>
-                <ul className="sub-menu">
-                  <li><a href="single-video.html">Video Post</a></li>
-                  <li><a href="single-audio.html">Audio Post</a></li>
-                  <li><a href="single-gallery.html">Gallery Post</a></li>
-                  <li><a href="single-standard.html">Standard Post</a></li>
-                </ul>
-              </li>
-              <li><a href="styles.html" title="">Styles</a></li>
-              <li><a href="page-about.html" title="">About</a></li>
-              <li><a href="page-contact.html" title="">Contact</a></li>
-            </ul>
-
-            <ul className="header__social">
-              <li className="ss-facebook">
-                <a href="https://facebook.com/">
-                  <span className="screen-reader-text">Facebook</span>
-                </a>
-              </li>
-              <li className="ss-twitter">
-                <a href="#0">
-                  <span className="screen-reader-text">Twitter</span>
-                </a>
-              </li>
-              <li className="ss-dribbble">
-                <a href="#0">
-                  <span className="screen-reader-text">Dribbble</span>
-                </a>
-              </li>
-              <li className="ss-pinterest">
-                <a href="#0">
-                  <span className="screen-reader-text">Behance</span>
-                </a>
-              </li>
-            </ul>
-
-          </nav>
-
-          <a href="#0" className="header__menu-toggle">
-            <span>Menu</span>
-          </a>
-
-        </header>
-
+        <Header />
         <Search />
         <Component {...pageProps} />
 
