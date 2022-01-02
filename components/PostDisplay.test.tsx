a16add26f71158e93357742913a03ec8846d2756
Curtis Timson <curt@live.co.uk>
2022-01-02 21:13:48 +0000

    add PostDisplay

---
+++ components/PostDisplay.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Post } from '../types/Post';
+import PostDisplay from './PostDisplay';
+
+describe('PostDisplay', () => {
+  describe('Posts', () => {
+    const post: Post = {
+      title: 'Post Title A', date: new Date('2022-01-30'), slug: 'a', tags: ['foo-tag', 'bar-tag', 'baz-tag'],
+    };
+
+    beforeEach(() => {
+      render(<PostDisplay post={post} />);
+    });
+
+    it('renders post title', () => {
+      expect(screen.getByText(post.title)).toBeInTheDocument();
+    });
+
+    it('renders first tag twice', () => {
+      expect(screen.getAllByText('foo-tag')).toHaveLength(2);
+    });
+
+    it('renders second tag twice', () => {
+      expect(screen.getAllByText('bar-tag')).toHaveLength(2);
+    });
+
+    it('renders third tag once', () => {
+      expect(screen.getAllByText('baz-tag')).toHaveLength(1);
+    });
+  });
+});
