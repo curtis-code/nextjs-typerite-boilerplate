bef224b813cb3b184a6fbb3a3d66fec68f8b7044
Curtis Timson <curt@live.co.uk>
2022-01-02 11:25:21 +0000

    add index.test.tsx

---
+++ pages/index.test.tsx
@@ -0,0 +1,20 @@
+import { getStaticProps } from '.';
+import { config } from '../config';
+import * as getStaticPropsForPosts from '../util/getStaticPropsForPosts';
+
+describe('index', () => {
+  describe('getStaticProps', () => {
+    let mockGetStaticPropsForPosts: any;
+
+    beforeAll(() => {
+      mockGetStaticPropsForPosts = jest.spyOn(getStaticPropsForPosts, 'getStaticPropsForPosts');
+      getStaticProps();
+    });
+
+    it('should call getStaticPropsForPosts', () => {
+      expect(mockGetStaticPropsForPosts).toHaveBeenCalledWith({
+        postsPerPage: config.postsPerPage,
+      });
+    });
+  });
+});
