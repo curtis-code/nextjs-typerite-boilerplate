8472f673cf9f7b3491fc1854772885e71df8c14b
Curtis Timson <curt@live.co.uk>
2022-01-09 15:40:53 +0000

    fix linting issues

--- util/getFormatClass.test.ts
+++ util/getFormatClass.test.ts
@@ -1,6 +1,6 @@
-import { Post } from "../types/Post"
-import { generateTestPost } from "../__tests__/generateTestPost.test"
-import { getFormatClass } from "./getFormatClass";
+import { Post } from '../types/Post';
+import { generateTestPost } from '../__tests__/generateTestPost.test';
+import { getFormatClass } from './getFormatClass';
 
 describe('getFormatClass', () => {
   const standardPost: Post = generateTestPost();
@@ -13,23 +13,23 @@ describe('getFormatClass', () => {
     const post: Post = {
       ...standardPost,
       imageList: ['/foo.png', '/bar.png'],
-    }
+    };
     expect(getFormatClass(post)).toEqual('format-gallery');
   });
 
   it('returns video format', () => {
     const post: Post = {
       ...standardPost,
-      videoUrl: 'https://videourl'
-    }
+      videoUrl: 'https://videourl',
+    };
     expect(getFormatClass(post)).toEqual('format-video');
   });
 
   it('returns audio format', () => {
     const post: Post = {
       ...standardPost,
-      audioUrl: 'https://audiourl'
-    }
+      audioUrl: 'https://audiourl',
+    };
     expect(getFormatClass(post)).toEqual('format-audio');
   });
-})
+});
