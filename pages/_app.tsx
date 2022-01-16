c46000edcd82e4f30ca9ccced91c49d5ddddd9f9
Curtis Timson <curt@live.co.uk>
2022-01-16 15:05:31 +0000

    move css to js import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import type { AppProps } from 'next/app';
+import '../styles/base.css';
+import '../styles/vendor.css';
+import '../styles/main.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
