239c2309176aed2be2573fd453966de858d21e1c
Curtis Timson <curt@live.co.uk>
2022-01-16 15:14:04 +0000

    split sociallinks sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import type { AppProps } from 'next/app';
-import '../styles/base.scss';
-import '../styles/vendor.scss';
-import '../styles/main.scss';
+import '../styles/style.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
